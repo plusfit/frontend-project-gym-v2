8fcafbde81ef21520ded5cc8d5cd279eec15ca63
nahuelgilvangwe <nahuel@vangwe.com>
2024-10-16 21:12:11 -0300

    feat: login feature added.

---
+++ src/app/features/auth/components/login-form/login-form.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Observable, Subject, takeUntil } from 'rxjs';
+
+import { Actions, ofActionSuccessful, Store } from '@ngxs/store';
+import { Router } from '@angular/router';
+import { AuthState } from '../../state/auth.state';
+import { Login } from '../../state/auth.actions';
+import { passwordValidator } from '@core/validators/password.validator';
+import { SnackBarService } from '@core/services/snackbar.service';
+import { AsyncPipe, NgClass } from '@angular/common';
+import { BtnDirective } from '@shared/directives/btn/btn.directive';
+import { ConditionalTextPipe } from '@shared/pipes/conditional-text.pipe';
+import { InputDirective } from '@shared/directives/btn/input.directive';
+
+@Component({
+  selector: 'app-login-form',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgClass,
+    BtnDirective,
+    AsyncPipe,
+    ConditionalTextPipe,
+    InputDirective,
+  ],
+  templateUrl: './login-form.component.html',
+  styleUrl: './login-form.component.css',
+})
+export class LoginFormComponent implements OnInit, OnDestroy {
+  loginForm!: FormGroup;
+
+  showPassword = false;
+  private destroy = new Subject<void>();
+
+  loading$: Observable<boolean> = this.store.select(AuthState.authLoading);
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private store: Store,
+    private actions: Actions,
+    private snackbar: SnackBarService,
+  ) {}
+
+  ngOnInit(): void {
+    // Create the login form with email and password fields
+    this.loginForm = this.fb.group({
+      identifier: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required, passwordValidator()]],
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy.next();
+    this.destroy.complete();
+  }
+
+  /**
+   * Authenticate the user and redirect to dashboard
+   * @returns void
+   */
+  login(): void {
+    if (this.loginForm.valid) {
+      this.store.dispatch(new Login(this.loginForm.value));
+      this.actions
+        .pipe(ofActionSuccessful(Login), takeUntil(this.destroy))
+        .subscribe(() => {
+          this.router.navigate(['/']);
+          this.snackbar.showSuccess('Login successful', 'OK');
+        });
+    }
+  }
+}
---
+++ src/app/shared/components/snackbar/snackbar.component.ts
@@ -0,0 +1,55 @@
+import { Component, Inject } from '@angular/core';
+import {
+  MAT_SNACK_BAR_DATA,
+  MatSnackBarRef,
+} from '@angular/material/snack-bar';
+import { ESeverity, SeverityIcon } from '@shared/enums/severity.enum';
+import { SnackBar } from '@shared/interfaces/snackbar.interface';
+import {NgClass} from "@angular/common";
+
+/**
+ * The SnackBarComponent displays a snackbar with a message and an icon based on the specified severity level.
+ */
+@Component({
+  selector: 'app-snackbar',
+  templateUrl: './snackbar.component.html',
+  styleUrls: ['./snackbar.component.scss'],
+  standalone: true,
+  imports: [
+    NgClass
+  ]
+})
+export class SnackBarComponent {
+  /**
+   * The severity of the snackbar.
+   */
+  severity: typeof ESeverity = ESeverity;
+  /**
+   * The CSS class for the severity of the snackbar.
+   */
+  severityClass = `alert-${this.severity.INFO}`;
+  /**
+   * The icon for the severity of the snackbar.
+   */
+  icon = '';
+
+  /**
+   * Creates an instance of the SnackBarComponent class.
+   * @param {MAT_SNACK_BAR_DATA} data - The data for the snackbar.
+   * @param {MatSnackBarRef<SnackBarComponent>} snackRef - The reference to the snackbar.
+   */
+  constructor(
+    @Inject(MAT_SNACK_BAR_DATA) public data: SnackBar,
+    private snackRef: MatSnackBarRef<SnackBarComponent>,
+  ) {
+    this.severityClass = `alert-${data.severity}`;
+    this.icon = SeverityIcon[data.severity];
+  }
+
+  /**
+   * Closes the snackbar.
+   */
+  close(): void {
+    this.snackRef.dismiss();
+  }
+}
