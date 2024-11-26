0a5935dd042598cb097641f432fb002804b8792e
Nico Gilardoni <nicogilardonik@gmail.com>
2024-11-26 13:27:18 -0300

    feat: :sparkles: Enhance routine form with improved title display and integrate table component for exercises

--- src/app/features/routines/pages/add-edit-page/add-edit-routine.page.component.ts
+++ src/app/features/routines/pages/add-edit-page/add-edit-routine.page.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 import { RoutineFormComponent } from '@features/routines/components/routine-form/routine-form.component';
+import { TableComponent } from '@shared/components/table/table.component';
+
 @Component({
   selector: 'app-example',
   standalone: true,
-  imports: [RoutineFormComponent],
+  imports: [RoutineFormComponent, TableComponent],
   templateUrl: './add-edit-routine.page.component.html',
   styleUrl: './add-edit-routine.page.component.css',
 })
-export class AddEditRoutinePageComponent {}
+export class AddEditRoutinePageComponent {
+  constructor() {}
+  displayedColumns: string[] = [
+    'name',
+    'isCustom',
+    'day',
+    'exercises',
+    'actions',
+  ];
+}
