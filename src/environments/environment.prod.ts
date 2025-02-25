929517d08eec90d404f9cd9da696ab1e5f8f6188
Nico Gilardoni <nicogilardonik@gmail.com>
2025-02-25 18:47:14 -0300

    feat: update angular.json and environment.prod.ts for improved configuration and API URL

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,21 @@
 export const environment = {
   production: true,
   env: 'Production',
-  apiUrl: 'http://localhost:4200/api',
+  api: 'https://backend-project-gym-production.up.railway.app/',
   exerciseTableLimit: 5,
   exerciseTableLimitOptions: [8, 5],
   routineTableLimit: 8,
   routineTableLimitOptions: [8, 5],
   clientsTableLimit: 8,
   clientsTableLimitOptions: [8, 5],
+  config: {
+    /**
+     * The number of items to display per page.
+     */
+    pageSize: 8,
+    /**
+     * The size of the grid for the application.
+     */
+    gridSize: 8,
+  },
 };
