49d6bb012f39dcf2fb3cd8e9b8764bb4a43fbb50
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2025-03-24 14:42:17 -0300

    feat: :sparkles: refactor: Add slide-in animation to text elements in card-routine component

--- src/app/features/screenRoutine/components/card-routine/card-routine.component.ts
+++ src/app/features/screenRoutine/components/card-routine/card-routine.component.ts
@@ -1,12 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  QueryList,
+  ViewChildren,
+} from '@angular/core';
 
 @Component({
   selector: 'app-card-routine',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './card-routine.component.html',
   styleUrl: './card-routine.component.css',
 })
-export class CardRoutineComponent {
+export class CardRoutineComponent implements AfterViewInit {
   @Input() routine: any;
+  @Input() backgroundColor: string | undefined;
+  @ViewChildren('exerciseText') exerciseTexts!: QueryList<ElementRef>;
+
+  ngAfterViewInit() {
+    setInterval(() => {
+      this.applyAnimation();
+    }, 60000);
+  }
+
+  applyAnimation() {
+    this.exerciseTexts.forEach((textElement, index) => {
+      const element = textElement.nativeElement;
+      element.style.animationDelay = `${index * 0.5}s`; // Retraso progresivo
+      element.classList.add('slide-text'); // Aplica la animación
+    });
+  }
 }
