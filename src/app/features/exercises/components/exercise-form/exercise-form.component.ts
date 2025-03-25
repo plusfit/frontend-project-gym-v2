315f8aeba3cfa4c39ee02dc5b46777031a6d3b75
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2025-03-24 22:08:09 -0300

    feat: :sparkles: refactor: Add lazy loading for image selection in exercise form

--- src/app/features/exercises/components/exercise-form/exercise-form.component.ts
+++ src/app/features/exercises/components/exercise-form/exercise-form.component.ts
@@ -1,5 +1,12 @@
 import { CommonModule, NgIf } from '@angular/common';
-import { Component, Inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import {
   FormBuilder,
   FormControl,
@@ -30,6 +37,8 @@ import { MatSelectModule } from '@angular/material/select';
 import { InputComponent } from '@shared/components/input/input.component';
 import { TextAreaComponent } from '@shared/components/text-area/text-area.component';
 import { TitleComponent } from '@shared/components/title/title.component';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-exercise-form',
@@ -48,6 +57,8 @@ import { TitleComponent } from '@shared/components/title/title.component';
     InputComponent,
     TextAreaComponent,
     TitleComponent,
+    MatAutocompleteModule,
+    MatInputModule,
   ],
 })
 export class ExerciseFormComponent implements OnInit, OnDestroy {
@@ -68,10 +79,37 @@ export class ExerciseFormComponent implements OnInit, OnDestroy {
   private destroy = new Subject<void>();
   title = 'Agregar ejercicio';
   btnTitle = 'Crear';
+  selectedFile: File | null = null;
+
+  @ViewChild('fileInput') fileInput!: ElementRef<HTMLInputElement>;
+  filePreview: string | ArrayBuffer | null = null;
+  dragging = false;
 
   types = [
     { value: 'cardio', viewValue: 'Cardio' },
     { value: 'room', viewValue: 'Sala' },
+  ];
+
+  categories = [
+    { value: 'chest', viewValue: 'Pecho' },
+    { value: 'back', viewValue: 'Espalda' },
+    { value: 'shoulders', viewValue: 'Hombros' },
+    { value: 'biceps', viewValue: 'Bíceps' },
+    { value: 'triceps', viewValue: 'Tríceps' },
+    { value: 'forearms', viewValue: 'Antebrazos' },
+    { value: 'quadriceps', viewValue: 'Cuádriceps' },
+    { value: 'hamstrings', viewValue: 'Isquiotibiales' },
+    { value: 'glutes', viewValue: 'Glúteos' },
+    { value: 'calves', viewValue: 'Pantorrillas' },
+    { value: 'core', viewValue: 'Core' },
+    { value: 'lower_back', viewValue: 'Zona Lumbar' },
+
+    // Tipo de Movimiento
+    { value: 'push', viewValue: 'Empuje' },
+    { value: 'pull', viewValue: 'Tracción' },
+    { value: 'knee_dominant', viewValue: 'Dominante de Rodilla' },
+    { value: 'hip_dominant', viewValue: 'Dominante de Cadera' },
+    { value: 'stabilization', viewValue: 'Estabilización' },
   ];
 
   ngOnDestroy(): void {
@@ -83,7 +121,8 @@ export class ExerciseFormComponent implements OnInit, OnDestroy {
     this.exerciseForm = this.fb.group({
       name: ['', [Validators.required]],
       description: ['', [Validators.required]],
-      //gifUrl: [''],
+      gifUrl: [''],
+      categorie: ['', Validators.required],
       type: ['', Validators.required],
       //mode: ['', Validators.required],
       rest: ['', [Validators.required, Validators.min(1)]],
@@ -107,6 +146,79 @@ export class ExerciseFormComponent implements OnInit, OnDestroy {
           );
           if (exerciseEditing) this.setDataForEdit(exerciseEditing);
         });
+    }
+  }
+
+  onFileSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+
+      // Validar que sea un archivo GIF
+      if (!file.type.includes('gif')) {
+        alert('Solo se permiten archivos GIF.');
+        return;
+      }
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        // Convertir el archivo a Base64
+        const base64File = e.target?.result as string;
+
+        // Asignar el string Base64 al formulario
+        this.exerciseForm.patchValue({
+          gifUrl: base64File, // Aquí asignas el string Base64 al campo gifUrl
+        });
+
+        // Actualizar la vista previa (opcional)
+        this.filePreview = base64File;
+      };
+      reader.readAsDataURL(file); // Convertir el archivo a Base64
+    }
+  }
+
+  removeFile(event: Event): void {
+    event.preventDefault();
+    event.stopPropagation();
+    this.filePreview = null;
+
+    if (this.fileInput) {
+      this.fileInput.nativeElement.value = '';
+    }
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = true;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = false;
+  }
+
+  onDrop(event: DragEvent) {
+    event.preventDefault();
+    this.dragging = false;
+    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
+      this.processFile(event.dataTransfer.files[0]);
+    }
+  }
+
+  processFile(file: File) {
+    if (file.type === 'image/gif') {
+      this.selectedFile = file;
+
+      // Generar vista previa
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.filePreview = reader.result;
+      };
+      reader.readAsDataURL(file);
+
+      console.log('Archivo seleccionado:', file.name);
+    } else {
+      console.error('Formato no permitido. Solo GIF.');
     }
   }
 
@@ -218,5 +330,9 @@ export class ExerciseFormComponent implements OnInit, OnDestroy {
 
   get seriesControl(): FormControl {
     return this.exerciseForm.get('series') as FormControl;
+  }
+
+  get caterieControl(): FormControl {
+    return this.exerciseForm.get('categorie') as FormControl;
   }
 }
