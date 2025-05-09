327143b6c994c8b391e9bc05c527b9f062a9c3ef
nahuelgilvangwe <ngil17github@gmail.com>
2025-05-09 15:22:05 -0300

    style: enhance UI consistency by updating component layouts with gradient backgrounds, improved form styles, and responsive design adjustments

--- src/app/shared/components/title/title.component.ts
+++ src/app/shared/components/title/title.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { MatDivider } from '@angular/material/divider';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [MatDivider],
+  imports: [MatDivider, NgClass],
   templateUrl: './title.component.html',
   styleUrls: ['./title.component.css'],
 })
@@ -12,4 +13,5 @@ export class TitleComponent {
   //TODO:
   @Input() title!: string;
   @Input() divider = true;
+  @Input() gradient = true;
 }
