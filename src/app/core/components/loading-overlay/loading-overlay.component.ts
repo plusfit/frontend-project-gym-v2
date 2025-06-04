ab0a3628c7bd0cae45a4d3745e98ee9513fcd1e9
nahuelgilvangwe <ngil17github@gmail.com>
2025-06-04 19:53:36 -0300

    refactor: add AsyncPipe to loading overlay component imports and clean up organization detail routines template for improved readability

--- src/app/core/components/loading-overlay/loading-overlay.component.ts
+++ src/app/core/components/loading-overlay/loading-overlay.component.ts
@@ -1,13 +1,14 @@
 /* eslint-disable prettier/prettier */
 
 import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
 import { LoadingOverlayService } from '@core/services/loading-overlay.service';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-loading-overlay',
   standalone: true,
-  imports: [MatProgressSpinnerModule],
+  imports: [MatProgressSpinnerModule, AsyncPipe],
   templateUrl: './loading-overlay.component.html',
   styleUrl: './loading-overlay.component.css',
 })
